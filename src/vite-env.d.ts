09774b0daffc85e97ce835ce5c95ff15912be3f0
arshdeep kaur <arshdeepkaur24@navgurukul.org>
2026-09-23 10:28:10 +0530

    Update project changes

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module "swiper/css" {
+  const css: string
+  export default css
+}
+
+declare module "swiper/css/*" {
+  const css: string
+  export default css
+}
